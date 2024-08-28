5d72f8a32d3e9c9b23bc0f78360bd5517a944aad
Evgenia <evgen1agerasimenko@yandex.ru>
2024-08-29 01:18:29 +0300

    Спринт 14

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,12 +1,48 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.service.ItemService;
+
+import java.util.List;
 
 /**
  * TODO Sprint add-controllers.
  */
 @RestController
 @RequestMapping("/items")
+@AllArgsConstructor
+@Validated
 public class ItemController {
+
+    private ItemService itemService;
+    private final String header = "X-Sharer-User-Id";
+
+    @PostMapping
+    public Item postItem(@RequestHeader(header) Long userId, @Valid @RequestBody Item item) {
+        return itemService.postItem(userId, item);
+    }
+
+    @PatchMapping("/{itemId}")
+    public Item updateItem(@RequestHeader(header) Long userId, @PathVariable Long itemId, @RequestBody Item item) {
+        return itemService.updateItem(userId, itemId, item);
+    }
+
+    @GetMapping("/{itemId}")
+    public Item getItemById(@PathVariable Long itemId) {
+        return itemService.getItemById(itemId);
+    }
+
+    @GetMapping
+    public List<Item> getOwnersItems(@RequestHeader(header) Long userId) {
+        return itemService.getOwnersItems(userId);
+    }
+
+    @GetMapping("/search")
+    public List<Item> searchItemByNameOrDescription(@RequestParam("text") String text) {
+        return itemService.searchItemByNameOrDescription(text);
+    }
 }
--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,19 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class ItemDto {
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean available;
+    private Long owner;
 }
