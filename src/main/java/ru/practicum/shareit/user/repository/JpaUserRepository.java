b17d8349f712426579dfbb716f72112f1fd4b0f0
Evgenia <evgen1agerasimenko@yandex.ru>
2024-09-30 22:33:08 +0300

    Спринт 16

--- src/main/java/ru/practicum/shareit/user/repository/JpaUserRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.practicum.shareit.user.model.User;
-
-public interface JpaUserRepository extends JpaRepository<User, Long> {
-}
